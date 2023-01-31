fe9c59c8129db54ef69716319e551b16c0918b65
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2023-01-31 12:24:01 +0300

    1.8: unicafe step3

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -1,4 +1,26 @@
 import { useState } from 'react'
+
+const Statistic = ({good,neutral,bad}) => {
+  return(
+    <div>
+        <p style={{marginRight:20}}>Good:{good}</p>
+        <p style={{marginRight:20}}>Neutral:{neutral}</p>
+        <p>Bad:{bad}</p>
+        <p>All:{good+bad+neutral}</p>
+        <p>Average:{(good+bad*-1)/(good+bad+neutral)}</p>
+        <p>Positive:{(good/(good+bad+neutral))*100} %</p>        
+    </div>      
+  )    
+} 
+
+const Button = (props) => {
+  return(
+    <button style={{marginRight:20}} onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -8,20 +30,6 @@ const App = () => {
     return(
       <h2>give feedback</h2>
     )    
-  }
-
-  const Statistic = () => {
-    return(
-      <h2>statistic</h2>
-    )    
-  } 
-
-  const Button = (props) => {
-    return(
-      <button style={{marginRight:20}} onClick={props.onClick}>
-        {props.text}
-      </button>
-    )
   }
 
   const increaseButton = (event) =>{
@@ -37,19 +45,12 @@ const App = () => {
     <div>
       <Title/>      
       <div>
-        <Button onClick = {()=>increaseButton("good")} text = "good"></Button>
-        <Button onClick = {()=>increaseButton("neutral")} text = "neutral"></Button>
-        <Button onClick = {()=>increaseButton("bad")} text = "bad"></Button>
-      </div>   
-      <Statistic/>
-      <div>
-        <p style={{marginRight:20}}>Good:{good}</p>
-        <p style={{marginRight:20}}>Neutral:{neutral}</p>
-        <p>Bad:{bad}</p>
-        <p>All:{good+bad+neutral}</p>
-        <p>Average:{(good+bad*-1)/(good+bad+neutral)}</p>
-        <p>Positive:{(good/(good+bad+neutral))*100} %</p>        
-      </div>         
+        <Button onClick = {()=>increaseButton("good")} text = "good"/>
+        <Button onClick = {()=>increaseButton("neutral")} text = "neutral"/>
+        <Button onClick = {()=>increaseButton("bad")} text = "bad"/>
+      </div> 
+      <h2>statistic</h2>  
+      <Statistic good={good} neutral={neutral} bad={bad}/>         
     </div>
   );
 }
