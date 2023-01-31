9b0e33d3d6626ab1869952a7bb2814ff482a3ebf
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2023-01-31 17:56:40 +0300

    1.12*: anecdotes step1

--- part01/anecdotes/src/index.jsx
+++ part01/anecdotes/src/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Years name = "Orhan" year = {33}/>
   </React.StrictMode>
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
